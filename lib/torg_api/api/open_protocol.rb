3e661c2d4cba19f2d910bf253c6a66a296751451
kirillvs <kirillvs@mail.ru>
2017-08-14 16:56:58 +1000

    create open protocol in torg

---
+++ lib/torg_api/api/open_protocol.rb
@@ -0,0 +1,63 @@
+module TorgApi
+  module Api
+    # Протокол вскрытия
+    class OpenProtocol < TorgApi::Base
+      # @return [Integer] id
+      attr_accessor :id
+
+      # @return [Integer] ID закупки
+      attr_accessor :tender_id
+
+      # @return [Integer] ID секретаря
+      attr_accessor :clerk_id
+
+      # @return [String] Место проведения
+      attr_accessor :location
+
+      # @return [Integer] Номер протокола
+      attr_accessor :num
+
+      # @return [String] Дата составления протокола
+      attr_accessor :open_date
+
+      # @return [String] Решение комиссии
+      attr_accessor :resolve
+
+      # @return [String] Город
+      attr_accessor :sign_city
+
+      # @return [String] Дата подписания протокола
+      attr_accessor :sign_date
+
+      # @return [Integer] ID комиссии
+      attr_accessor :commission_id,
+
+      class << self
+        # Создает протокол вскрытия в системе
+        # @param tender_id [Integer] id закупки
+        # @param params [Hash] Параметры протокола вскрытия B2B-Center
+        # @return [OpenProtocol]
+        def create(tender_id, params)
+          responce = JSON.parse(
+            torg_resource["tenders/#{tender_id}/open_protocols"].post(
+              open_protocol: {
+                tender_id: tender_id,
+                num: params[:number],
+                open_date: params[:date],
+                sign_city: params[:place],
+                sign_date: params[:sign_protocol_date],
+                compound_open_date: params[:procedure_stage_date]
+              },
+              accept: :json,
+              content_type: :json,
+              format: :json
+            ),
+            symbolize_names: true
+          )
+
+          new(responce)
+        end
+      end
+    end
+  end
+end
